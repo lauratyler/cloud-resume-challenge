16f9e28dbd86a55ae68bfbec8d7f30a1f59d450d
lauratyler <laurabtyler22@gmail.com>
2026-05-18 11:13:11 -0500

    feat: add portfolio bucket

--- infra/lambda/index.js
+++ infra/lambda/index.js
@@ -1,58 +1,24 @@
-const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-const { DynamoDBDocumentClient, GetCommand, UpdateCommand } = require("@aws-sdk/lib-dynamodb");
-
-const client = new DynamoDBClient({
-    region: "us-east-2"
-});
-
-const dynamodb = DynamoDBDocumentClient.from(client);
+const { handler: visitorCount } = require("./functions/visitor-count")
+const { handler: docs } = require("./functions/docs")
 
 exports.handler = async (event) => {
     try {
-        const method = event?.requestContext?.http?.method;
+        const path = event?.requestContext?.http?.path;
 
-        if (method === "GET") {
-            const result = await dynamodb.send(
-                new GetCommand({
-                    TableName: "VisitorCount",
-                    Key: { id: "visitor_count" }
-                })
-            );
-
-            return {
-                statusCode: 200,
-                body: JSON.stringify({ count: result.Item?.count || 0 })
-            };
+        if (path === "/visitor-count") {
+            return await visitorCount(event)
         }
 
-        if (method === "POST") {
-            const result = await dynamodb.send(
-                new UpdateCommand({
-                    TableName: "VisitorCount",
-                    Key: { id: "visitor_count" },
-                    UpdateExpression: "SET #c = if_not_exists(#c, :start) + :inc",
-                    ExpressionAttributeNames: { "#c": "count" },
-                    ExpressionAttributeValues: {
-                        ":inc": 1,
-                        ":start": 0
-                    },
-                    ReturnValues: "UPDATED_NEW"
-                })
-            );
-
-            return {
-                statusCode: 200,
-                body: JSON.stringify({ count: result.Attributes.count })
-            };
+        if (path === "/docs") {
+            return await docs(event)
         }
 
-        return { statusCode: 405, body: "Method Not Allowed" };
-
+        return { statusCode: 404, body: `Not Found: ${path}` }
     } catch (err) {
-        console.error("ERROR:", err);
+        console.error("ERROR:", err)
         return {
             statusCode: 500,
             body: JSON.stringify({ error: err.message })
         };
     }
-};
+}
